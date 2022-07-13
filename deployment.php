b3b6d6c117e5d759a2421f9693af02ad6b8bd08e
Muntasib-creator <muntasibchowdhury@gmail.com>
2022-07-13 09:33:16 +0600

    run set and create report

--- deployment.php
+++ deployment.php
@@ -9,13 +9,13 @@
     $user = mysqli_fetch_all($res,MYSQLI_ASSOC)[0];
     if($user["run_status"]!="no"){
         $tc_id = explode("_", $user["run_status"])[1];
-        $action_q = "SELECT * FROM actions WHERE tc_id = '$tc_id' ORDER BY `actions`.`action_seq` ASC";
+        // $action_q = "SELECT * FROM actions WHERE tc_id IN (" . implode(',', json_encode($tc_id)) . ") ORDER BY `actions`.`action_seq` ASC";
+        $action_q = "SELECT * FROM actions WHERE tc_id IN (" . substr($tc_id,1,-1) . ") ORDER BY `actions`.`tc_id` ASC;";
         $res = mysqli_query($conn, $action_q);
         $action_res = mysqli_fetch_all($res,MYSQLI_ASSOC);
-        $tc_q = "SELECT * FROM testcases WHERE id = '$tc_id'";
+        $tc_q = "SELECT * FROM testcases WHERE id IN (" . substr($tc_id,1,-1) . ");";
         $tc_res = mysqli_query($conn, $tc_q);
-        $tc_res = mysqli_fetch_all($tc_res,MYSQLI_ASSOC)[0];
-
+        $tc_res = mysqli_fetch_all($tc_res,MYSQLI_ASSOC);
 
         $response = [
             "run_status" => $user["run_status"],
@@ -31,7 +31,6 @@
             "run_status" => "no",
         ];
         echo json_encode($response);
-
     }
 
 
