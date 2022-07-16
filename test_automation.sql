9da6a33ac11ee57dc52c238e1ae7d4652ecdf279
Muntasib-creator <muntasibchowdhury@gmail.com>
2022-07-16 13:23:29 +0600

    db_backup

--- test_automation.sql
+++ test_automation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 12, 2022 at 10:13 AM
+-- Generation Time: Jul 16, 2022 at 09:22 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -54,13 +54,15 @@ INSERT INTO `actions` (`id`, `tc_id`, `action_seq`, `action_name`, `action_disab
 (35, 9, 2, 'temp_name', 0, 2, 'click', 'selenium action', 'click'),
 (36, 9, 3, 'temp_name', 0, 1, 'att_name', 'selenium action', 'att_val'),
 (37, 9, 3, 'temp_name', 0, 2, 'text', 'selenium action', 'val222'),
-(38, 23, 1, 'temp_name', 0, 1, 'go to link', 'selenium action', 'https://demo.zeuz.ai/web/level/one/scenerios/login'),
-(39, 23, 2, 'temp_name', 0, 1, 'go to link', 'selenium action', 'asdasd'),
-(120, 25, 1, 'temp_name aa', 0, 1, 'go to link', 'selenium action', 'https://demo.zeuz.ai/web/level/one/scenerios/login'),
-(121, 25, 2, 'abcd', 1, 1, 'id', 'element parameter', 'username_id'),
-(122, 25, 2, 'abcd', 1, 2, 'text', 'selenium action', 'zeuzTest'),
-(123, 25, 3, 'temp_name', 1, 1, 'id', 'element parameter', 'password_id'),
-(124, 25, 3, 'temp_name', 1, 2, 'text', 'selenium action', 'zeuzPass');
+(143, 25, 1, 'temp_name aa', 0, 1, 'go to link', 'selenium action', 'https://demo.zeuz.ai/web/level/one/scenerios/login'),
+(144, 25, 2, 'abcd', 0, 1, 'id', 'element parameter', 'username_id'),
+(145, 25, 2, 'abcd', 0, 2, 'text', 'selenium action', 'zeuzTest'),
+(146, 25, 3, 'temp_name', 1, 1, 'id', 'element parameter', 'password_id'),
+(147, 25, 3, 'temp_name', 1, 2, 'text', 'selenium action', 'zeuzPass'),
+(148, 25, 4, 'None', 0, 1, 'sleep', 'common action', '4'),
+(152, 0, 1, 'temp_name', 0, 1, 'go to link', 'selenium action', 'https://demo.zeuz.ai/web/level/one/scenerios/login'),
+(162, 23, 1, 'temp_name', 0, 1, 'go to link', 'selenium action', 'https://demo.zeuz.ai/web/level/one/scenerios/login'),
+(163, 23, 2, 'None', 0, 1, 'go to link', 'selenium action', 'https://demo.zeuz.ai/web/level/one/scenerios/login');
 
 -- --------------------------------------------------------
 
@@ -82,10 +84,10 @@ CREATE TABLE `testcases` (
 --
 
 INSERT INTO `testcases` (`id`, `tc_name`, `tc_obj`, `tc_creation_date`, `tc_result`, `tc_duration`) VALUES
-(9, 'sidebar', 'obj', '2022-06-01 18:18:46', 'Not run yet', '0.00.00'),
-(15, 'dashboard', 'to test dashboard', '2022-06-01 18:29:50', 'Not run yet', '0.00.00'),
-(23, 'a2', 'a2', '2022-06-02 12:29:51', 'Blocked', '0:02:40'),
-(25, 'login test', 'to test login', '2022-06-19 21:16:19', 'Passed', '0:00:08');
+(9, 'sidebar', 'obj', '2022-06-01 18:18:46', 'Blocked', '0:02:07'),
+(15, 'dashboard', 'to test dashboard', '2022-06-01 18:29:50', 'Passed', '0:00:00'),
+(23, 'a2', 'a2', '2022-06-02 12:29:51', 'Passed', '0:01:06'),
+(25, 'login test', 'to test login', '2022-06-19 21:16:19', 'Passed', '0:02:51');
 
 -- --------------------------------------------------------
 
@@ -99,15 +101,17 @@ CREATE TABLE `users` (
   `password` varchar(65) NOT NULL,
   `api-key` varchar(30) NOT NULL,
   `policy` varchar(15) NOT NULL DEFAULT 'tester',
-  `run_status` varchar(10) NOT NULL
+  `run_status` varchar(300) NOT NULL DEFAULT 'no',
+  `loggedin` varchar(10) NOT NULL DEFAULT 'false'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `api-key`, `policy`, `run_status`) VALUES
-(1, 'admin', '$2y$10$bNIHFiXqtktpoo82iBdBVekjjIqxbFHNAqaUgJWj.sxPR9ecPbOgC', 'ntl1MC5NcdW7&kj8Pm70MmVx02A#nX', 'admin', 'no');
+INSERT INTO `users` (`id`, `username`, `password`, `api-key`, `policy`, `run_status`, `loggedin`) VALUES
+(1, 'admin', '$2y$10$bNIHFiXqtktpoo82iBdBVekjjIqxbFHNAqaUgJWj.sxPR9ecPbOgC', 'ntl1MC5NcdW7&kj8Pm70MmVx02A#nX', 'admin', 'no', 'true'),
+(8, 'test', '$2y$10$JKA.tnSlvnBXAHxajEpNjuimAqN6./MC6lWuYRokVye1EPafeq.6y', 'yTLIkJ}l3FbMU]nN4Z0<}cQ$0%myBu', 'tester', 'no', 'false');
 
 --
 -- Indexes for dumped tables
@@ -139,19 +143,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `actions`
 --
 ALTER TABLE `actions`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=164;
 
 --
 -- AUTO_INCREMENT for table `testcases`
 --
 ALTER TABLE `testcases`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
